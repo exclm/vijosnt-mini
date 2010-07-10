7b249f467bee787898166b53ff038288db00fdb6
iceboy <iceboy@sjtu.edu.cn>
2010-07-10 11:00:31 +0000

    added class StreamPipe

---
+++ Utility/StreamPipe.vb
@@ -0,0 +1,43 @@
+﻿Public NotInheritable Class StreamPipe
+    Inherits WaitHandle
+
+    Private m_Source As Stream
+    Private m_Target As Stream
+    Private m_Buffer As Byte()
+    Private m_BufferSize As Int32
+    Private m_Event As ManualResetEvent
+
+    Public Sub New(ByVal Source As Stream, ByVal Target As Stream)
+        Me.New(Source, Target, 4096)
+    End Sub
+
+    Public Sub New(ByVal Source As Stream, ByVal Target As Stream, ByVal BufferSize As Int32)
+        m_Source = Source
+        m_Target = Target
+        m_Buffer = New Byte(0 To BufferSize - 1) {}
+        m_BufferSize = BufferSize
+        m_Source.BeginRead(m_Buffer, 0, m_BufferSize, AddressOf OnRead, Nothing)
+        m_Event = New ManualResetEvent(False)
+
+        MyBase.SafeWaitHandle = m_Event.SafeWaitHandle
+    End Sub
+
+    Private Sub OnRead(ByVal ar As IAsyncResult)
+        Try
+            Dim Length As Int32 = m_Source.EndRead(ar)
+            If Length = 0 Then Throw New EndOfStreamException()
+            m_Target.BeginWrite(m_Buffer, 0, Length, AddressOf OnWrite, Nothing)
+        Catch ex As Exception
+            m_Event.Set()
+        End Try
+    End Sub
+
+    Private Sub OnWrite(ByVal ar As IAsyncResult)
+        Try
+            m_Target.EndWrite(ar)
+            m_Source.BeginRead(m_Buffer, 0, m_BufferSize, AddressOf OnRead, Nothing)
+        Catch ex As Exception
+            m_Event.Set()
+        End Try
+    End Sub
+End Class
