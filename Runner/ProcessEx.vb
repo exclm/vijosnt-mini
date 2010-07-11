5f6a39bb0dbae06c663314173e39392a89c9866f
iceboy <iceboy@sjtu.edu.cn>
2010-07-11 12:18:31 +0000

    added class ProcessEx

---
+++ Runner/ProcessEx.vb
@@ -0,0 +1,22 @@
+﻿Public NotInheritable Class ProcessEx
+    Inherits WaitHandle
+
+#Region "Shared members"
+    Public Shared Function Attach(ByVal ProcessID As Int32) As ProcessEx
+        Dim hProcess As IntPtr
+        hProcess = OpenProcess(ProcessAccess.PROCESS_ALL_ACCESS, False, ProcessID)
+        Return New ProcessEx(hProcess)
+    End Function
+#End Region
+
+    Private m_hProcess As IntPtr
+
+    Public Sub New(ByVal hProcess As IntPtr)
+        m_hProcess = hProcess
+        MyBase.SafeWaitHandle = New SafeWaitHandle(hProcess, True)
+    End Sub
+
+    Public Sub Kill(ByVal ReturnCode As Int32)
+        Win32True(TerminateProcess(m_hProcess, ReturnCode))
+    End Sub
+End Class
---
+++ Utility/NativeApi.vb
@@ -0,0 +1,25 @@
+﻿Module NativeApi
+    Public Declare Function NtQueryInformationProcess Lib "ntdll.dll" ( _
+        ByVal ProcessHandle As IntPtr, _
+        ByVal ProcessInformationClass As PROCESSINFOCLASS, _
+        ByRef ProcessInformation As PROCESS_BASIC_INFORMATION, _
+        ByVal ProcessInformationLength As Int32, _
+        ByRef ReturnLength As Int32) As NTSTATUS
+
+    Public Structure PROCESS_BASIC_INFORMATION
+        Dim Reserved0 As IntPtr
+        Dim PebBaseAddress As IntPtr
+        Dim Reserved1 As IntPtr
+        Dim Reserved2 As IntPtr
+        Dim UniqueProcessId As IntPtr
+        Dim Reserved3 As IntPtr
+    End Structure
+
+    Public Enum PROCESSINFOCLASS
+        ProcessBasicInformation = 0
+    End Enum
+
+    Public Enum NTSTATUS As Int32
+        STATUS_SUCCESS = 0
+    End Enum
+End Module
