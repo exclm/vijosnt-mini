799a19fc19d69ac624b1c9750b3d97e11ea78998
iceboy <iceboy@sjtu.edu.cn>
2010-08-03 06:51:36 +0000

    added Token.SetPrivilege, lessen abuse of the `Return Me pattern'

--- Win32/Token.vb
+++ Win32/Token.vb
@@ -3,6 +3,10 @@
 
     Protected m_Handle As IntPtr
 
+    Public Sub New()
+        Win32True(OpenProcessToken(GetCurrentProcess(), TokenAccess.TOKEN_ALL_ACCESS, m_Handle))
+    End Sub
+
     Public Sub New(ByVal UserName As String, ByVal Password As String)
         Win32True(LogonUser(UserName, ".", Password, _
             LogonType.LOGON32_LOGON_INTERACTIVE, LogonProvider.LOGON32_PROVIDER_DEFAULT, _
@@ -52,6 +56,21 @@
             Marshal.FreeHGlobal(TokenGroupsPtr)
         End Try
     End Function
+
+    Public Sub SetPrivilege(ByVal Privilege As String, ByVal EnablePrivilege As Boolean)
+        Dim TokenPrivileges As TOKEN_PRIVILEGES
+
+        Win32True(LookupPrivilegeValue(Nothing, Privilege, TokenPrivileges.Privilege.Luid))
+        TokenPrivileges.PrivilegeCount = 1
+
+        If EnablePrivilege Then
+            TokenPrivileges.Privilege.Attributes = PrivilegeAttribute.SE_PRIVILEGE_ENABLED
+        Else
+            TokenPrivileges.Privilege.Attributes = 0
+        End If
+
+        Win32True(AdjustTokenPrivileges(m_Handle, False, TokenPrivileges, Marshal.SizeOf(TokenPrivileges), Nothing, Nothing))
+    End Sub
 
 #Region "IDisposable Support"
     Private disposedValue As Boolean ' To detect redundant calls
