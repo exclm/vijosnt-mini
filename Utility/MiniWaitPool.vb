c549556cb614db722e7eea2f9744fd35c7bbf5d4
iceboy <iceboy@sjtu.edu.cn>
2010-08-04 04:39:10 +0000

    Used Date.Now.Ticks to calculate ticks

--- Utility/MiniWaitPool.vb
+++ Utility/MiniWaitPool.vb
@@ -1,18 +1,16 @@
 ﻿Public Class MiniWaitPool
-    ' TODO: Use GetTickCount64() instead of Date
-
     Public Delegate Sub WaitPoolCallback(ByVal State As Object, ByVal Timeouted As Boolean)
 
     Protected Structure WaitPoolEntry
-        Public Sub New(ByVal WaitHandle As WaitHandle, ByVal Timeout As Nullable(Of Date), ByVal Callback As WaitPoolCallback, ByVal CallbackState As Object)
+        Public Sub New(ByVal WaitHandle As WaitHandle, ByVal TimeoutTick As Nullable(Of Int64), ByVal Callback As WaitPoolCallback, ByVal CallbackState As Object)
             Me.WaitHandle = WaitHandle
-            Me.Timeout = Timeout
+            Me.TimeoutTick = TimeoutTick
             Me.Callback = Callback
             Me.CallbackState = CallbackState
         End Sub
 
         Dim WaitHandle As WaitHandle
-        Dim Timeout As Nullable(Of Date)
+        Dim TimeoutTick As Nullable(Of Int64)
         Dim Callback As WaitPoolCallback
         Dim CallbackState As Object
     End Structure
@@ -45,16 +43,16 @@
     End Sub
 
     Public Sub Add(ByVal WaitHandle As WaitHandle, ByVal TimeoutValue As Int32, ByVal Callback As WaitPoolCallback, ByVal State As Object)
-        Dim TimeoutDate As Nullable(Of Date)
+        Dim TimeoutTick As Nullable(Of Int64)
 
         If TimeoutValue <> Timeout.Infinite Then
-            TimeoutDate = Date.Now.Add(New TimeSpan(CType(TimeoutValue, Int64) * 10000))
+            TimeoutTick = Date.Now.Ticks + Math.BigMul(TimeoutValue, 10000)
         Else
-            TimeoutDate = Nothing
+            TimeoutTick = Nothing
         End If
 
         SyncLock m_SyncRoot
-            m_List.Add(New WaitPoolEntry(WaitHandle, TimeoutDate, Callback, State))
+            m_List.Add(New WaitPoolEntry(WaitHandle, TimeoutTick, Callback, State))
         End SyncLock
         m_Event.Set()
     End Sub
@@ -89,23 +87,23 @@
 
             If Entries.Length = 0 Then Exit While
 
-            Dim TimeoutDate As Nullable(Of Date) = Nothing
+            Dim TimeoutTick As Nullable(Of Int64) = Nothing
             Dim TimeoutSource As Int32 = -1
             Dim WaitHandles As WaitHandle() = New WaitHandle(0 To Entries.Length - 1) {}
 
             For Index As Int32 = 0 To Entries.Length - 1
                 WaitHandles(Index) = Entries(Index).WaitHandle
-                Dim Value As Nullable(Of Date) = Entries(Index).Timeout
-                If Value IsNot Nothing AndAlso (TimeoutDate Is Nothing OrElse Value < TimeoutDate) Then
-                    TimeoutDate = Value
+                Dim Tick As Nullable(Of Int64) = Entries(Index).TimeoutTick
+                If Tick IsNot Nothing AndAlso (TimeoutTick Is Nothing OrElse Tick < TimeoutTick) Then
+                    TimeoutTick = Tick
                     TimeoutSource = Index
                 End If
             Next
 
             Dim TimeoutValue As Int32 = Timeout.Infinite
 
-            If TimeoutDate IsNot Nothing Then
-                TimeoutValue = CType((TimeoutDate.Value - Now).TotalMilliseconds, Int32)
+            If TimeoutTick IsNot Nothing Then
+                TimeoutValue = CType((TimeoutTick - Date.Now.Ticks) \ 10000, Int32)
                 If TimeoutValue < 0 Then
                     DispatchEntry(Entries(TimeoutSource), True)
                     Continue While
