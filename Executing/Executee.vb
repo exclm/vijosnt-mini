50e3cee1a15450be3fbaba0dcc6661d208b3a25c
iceboy <iceboy@sjtu.edu.cn>
2010-08-10 11:52:50 +0000

    made Executee abstract class, accompanied with several grumbles

--- Executing/Executee.vb
+++ Executing/Executee.vb
@@ -2,38 +2,23 @@
 Imports VijosNT.Win32
 
 Namespace Executing
-    Friend Class Executee
-        Protected m_WaitPool As MiniWaitPool
-        Protected m_RequiredEnvironment As EnvironmentTag
-        Protected m_ApplicationName As String
-        Protected m_Callback As ExecuteCompletionCallback
-        Protected m_CallbackState As Object
+    Friend MustInherit Class Executee
+        Protected m_Environment As Environment
 
-        Public Sub New(ByVal WaitPool As MiniWaitPool, ByVal RequiredEnvironment As EnvironmentTag, ByVal ApplicationName As String, ByVal Callback As ExecuteCompletionCallback, ByVal State As Object)
-            m_WaitPool = WaitPool
-            m_RequiredEnvironment = RequiredEnvironment
-            m_ApplicationName = ApplicationName
-            m_Callback = Callback
-            m_CallbackState = State
-        End Sub
-
-        Public ReadOnly Property RequiredEnvironment() As EnvironmentTag
+        Public Property Environment() As Environment
             Get
-                Return m_RequiredEnvironment
+                Return m_Environment
             End Get
+
+            Set(ByVal Value As Environment)
+                m_Environment = Value
+            End Set
         End Property
 
-        Public Sub Execute(ByVal Environment As Environment)
-            Using Suspended As ProcessEx.Suspended = ProcessEx.CreateSuspended(m_ApplicationName, Nothing, Nothing, Nothing, Environment.DesktopName, Nothing, Nothing, Nothing, Environment.Token)
-                Dim Process As ProcessEx = Suspended.Resume()
-                m_WaitPool.SetWait(Process, Nothing, _
-                    Sub()
-                        If m_Callback IsNot Nothing Then
-                            m_Callback.Invoke(New ExecuteResult(m_CallbackState))
-                        End If
-                        Environment.Untake()
-                    End Sub, Nothing)
-            End Using
+        Public MustOverride ReadOnly Property RequiredEnvironment() As EnvironmentTag
+
+        Public Overridable Sub Execute()
+            Environment.Untake()
         End Sub
     End Class
 End Namespace
