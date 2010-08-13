fd185f4b972e3f9e0e47086141fc5b5df37b9459
iceboy <iceboy@sjtu.edu.cn>
2010-08-13 07:05:01 +0000

    entry grumble

--- Service/ServiceEntry.vb
+++ Service/ServiceEntry.vb
@@ -5,18 +5,20 @@ Imports VijosNT.Utility
 Friend NotInheritable Class ServiceEntry
     Public Shared Sub Main()
         SetWorkerThreads(40, 500)
-        Dim TestSuite As New APlusBTestSuite()
+
+        Dim ts As New APlusBTestSuite()
         Dim wd As New WatchDog
-        wd.Start()
         Dim pm As New ProcessMonitor
+        Dim ex As New Executor(4, New EnvironmentPool() {New TrustedEnvironmentPool()})
+
+        wd.Start()
         pm.Start()
-        With New Executor(1, New EnvironmentPool() {New TrustedEnvironmentPool()})
-            For Each TestCase As TestCase In TestSuite.TryLoad("A+B")
-                .Queue(New TestCaseExecutee(wd, pm, "D:\Works\C++\WinTest\x64\Release\WinTest.exe", TestCase, _
-                    Sub(Result As TestCaseExecuteeResult)
-                        Console.WriteLine("Test {0}: Score {1} Time {2}", Result.Index, Result.Score, Result.TimeQuotaUsage)
-                    End Sub, Nothing))
-            Next
-        End With
+
+        For Each tc As TestCase In ts.TryLoad("A+B")
+            ex.Queue(New TestCaseExecutee(wd, pm, "D:\Works\C++\WinTest\x64\Release\WinTest.exe", tc, _
+                Sub(rs As TestCaseExecuteeResult)
+                    Console.WriteLine("Test {0}: Score {1} Time {2}ms", rs.Index, rs.Score, rs.TimeQuotaUsage \ 10000)
+                End Sub, Nothing))
+        Next
     End Sub
 End Class
