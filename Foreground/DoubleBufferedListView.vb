a8a16cadb0e4ad71f133539ea147dfcd396ef4c8
iceboy <iceboy@sjtu.edu.cn>
2010-08-27 07:54:57 +0000

    readded double buffered ListView

---
+++ Foreground/DoubleBufferedListView.vb
@@ -0,0 +1,10 @@
+﻿Namespace Foreground
+    Public Class DoubleBufferedListView
+        Inherits ListView
+
+        Public Sub New()
+            MyBase.New()
+            DoubleBuffered = True
+        End Sub
+    End Class
+End Namespace
