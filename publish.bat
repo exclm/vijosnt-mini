5c0aaf7cc5c0e660109a167861defab82fcedd9d
iceboy <iceboy@sjtu.edu.cn>
2010-08-27 10:23:07 +0000

    grumbles

---
+++ publish.bat
@@ -0,0 +1,14 @@
+if not defined ProgramFiles(x86) set ProgramFiles(x86)=%ProgramFiles%
+"%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" /rebuild "Release|x86" "VijosNT Mini.sln"
+"%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" /rebuild "Release|x64" "VijosNT Mini.sln"
+del pub.rar
+rd pub /s /q
+md pub
+md pub\x86
+copy "bin\x86\release\VijosNT Mini.exe" pub\x86
+copy "%ProgramFiles(x86)%\SQLite.NET\bin\System.Data.SQLite.dll" pub\x86
+md pub\x64
+copy "bin\x64\release\VijosNT Mini.exe" pub\x64
+copy "%ProgramFiles(x86)%\SQLite.NET\bin\x64\System.Data.SQLite.dll" pub\x64
+start /wait "" "%ProgramFiles%\WinRAR\WinRAR.exe" a pub.rar pub
+rd pub /s /q
