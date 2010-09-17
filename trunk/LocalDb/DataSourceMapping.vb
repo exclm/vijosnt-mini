24675554ce7314d33ce33304e53156dfd8aa218f
iceboy@sjtu.edu.cn <iceboy@sjtu.edu.cn@cc5827d4-b6a0-e298-b568-16c0930e2a50>
2010-09-17 09:05:52 +0000

    ui support for namespace done

--- trunk/LocalDb/DataSourceMapping.vb
+++ trunk/LocalDb/DataSourceMapping.vb
@@ -9,9 +9,10 @@
 
         Shared Sub New()
             Using Command As SQLiteCommand = Database.CreateCommand( _
-                "CREATE TABLE IF NOT EXISTS DataSourceMapping (" & _
+                "CREATE TABLE IF NOT EXISTS DataSourceMapping2 (" & _
                 "Id INTEGER PRIMARY KEY AUTOINCREMENT, " & _
                 "ClassName TEXT, " & _
+                "Namespace TEXT, " & _
                 "Parameter TEXT, " & _
                 "PollingInterval INTEGER, " & _
                 "IpcAnnouncement TEXT, " & _
@@ -20,17 +21,17 @@
             End Using
 
             m_SelectCommand = Database.CreateCommand( _
-                "SELECT * FROM DataSourceMapping")
+                "SELECT * FROM DataSourceMapping2")
             m_SelectHeaderCommand = Database.CreateCommand( _
-                "SELECT Id, ClassName, Parameter FROM DataSourceMapping")
+                "SELECT Id, ClassName, Namespace, Parameter FROM DataSourceMapping2")
             m_SelectConfigCommand = Database.CreateCommand( _
-                "SELECT * FROM DataSourceMapping WHERE Id = @Id")
+                "SELECT * FROM DataSourceMapping2 WHERE Id = @Id")
             m_InsertCommand = Database.CreateCommand( _
-                "INSERT INTO DataSourceMapping (Id, ClassName, Parameter, PollingInterval, IpcAnnouncement, HttpAnnouncement) VALUES (NULL, @ClassName, @Parameter, @PollingInterval, @IpcAnnouncement, @HttpAnnouncement)")
+                "INSERT INTO DataSourceMapping2 (Id, ClassName, Namespace, Parameter, PollingInterval, IpcAnnouncement, HttpAnnouncement) VALUES (NULL, @ClassName, @Namespace, @Parameter, @PollingInterval, @IpcAnnouncement, @HttpAnnouncement)")
             m_DeleteCommand = Database.CreateCommand( _
-                "DELETE FROM DataSourceMapping WHERE Id = @Id")
+                "DELETE FROM DataSourceMapping2 WHERE Id = @Id")
             m_UpdateCommand = Database.CreateCommand( _
-                "UPDATE DataSourceMapping SET ClassName = @ClassName, Parameter = @Parameter, PollingInterval = @PollingInterval, IpcAnnouncement = @IpcAnnouncement, HttpAnnouncement = @HttpAnnouncement WHERE Id = @Id")
+                "UPDATE DataSourceMapping2 SET ClassName = @ClassName, Namespace = @Namespace, Parameter = @Parameter, PollingInterval = @PollingInterval, IpcAnnouncement = @IpcAnnouncement, HttpAnnouncement = @HttpAnnouncement WHERE Id = @Id")
         End Sub
 
         Public Shared Function GetAll() As IDataReader
@@ -58,10 +59,11 @@
             End Using
         End Function
 
-        Public Shared Sub Add(ByVal ClassName As String, ByVal Parameter As String, ByVal PollingInterval As Nullable(Of Int32), ByVal IpcAnnouncement As String, ByVal HttpAnnouncement As String)
+        Public Shared Sub Add(ByVal ClassName As String, ByVal [Namespace] As String, ByVal Parameter As String, ByVal PollingInterval As Nullable(Of Int32), ByVal IpcAnnouncement As String, ByVal HttpAnnouncement As String)
             Using Command As SQLiteCommand = m_InsertCommand.Clone()
                 With Command.Parameters
                     .AddWithValue("@ClassName", ClassName)
+                    .AddWithValue("@Namespace", [Namespace])
                     .AddWithValue("@Parameter", Parameter)
                     .AddWithValue("@PollingInterval", PollingInterval)
                     .AddWithValue("@IpcAnnouncement", IpcAnnouncement)
@@ -80,11 +82,12 @@
             End Using
         End Sub
 
-        Public Shared Sub Update(ByVal Id As Int32, ByVal ClassName As String, ByVal Parameter As String, ByVal PollingInterval As Nullable(Of Int32), ByVal IpcAnnouncement As String, ByVal HttpAnnouncement As String)
+        Public Shared Sub Update(ByVal Id As Int32, ByVal ClassName As String, ByVal [Namespace] As String, ByVal Parameter As String, ByVal PollingInterval As Nullable(Of Int32), ByVal IpcAnnouncement As String, ByVal HttpAnnouncement As String)
             Using Command As SQLiteCommand = m_UpdateCommand.Clone()
                 With Command.Parameters
                     .AddWithValue("@Id", Id)
                     .AddWithValue("@ClassName", ClassName)
+                    .AddWithValue("@Namespace", [Namespace])
                     .AddWithValue("@Parameter", Parameter)
                     .AddWithValue("@PollingInterval", PollingInterval)
                     .AddWithValue("@IpcAnnouncement", IpcAnnouncement)
