05c516e6b15ef893a40b69be3fdde76b4e2aa937
iceboy@sjtu.edu.cn <iceboy@sjtu.edu.cn@cc5827d4-b6a0-e298-b568-16c0930e2a50>
2010-10-01 05:41:05 +0000

    combined x86 and x64 configuration, modified publish.bat

--- trunk/publish.bat
+++ trunk/publish.bat
@@ -5,25 +5,23 @@ echo.
 echo Program Files Directory: %ProgramFiles%
 echo Program Files (x86) Directory: %ProgramFiles(x86)%
 echo.
-echo Compiling for x86 target...
-"%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" /rebuild "Release|x86" "VijosNT Mini.sln"
-echo Compiling for x64 target...
-"%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" /rebuild "Release|x64" "VijosNT Mini.sln"
+echo Compiling...
+"%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" /rebuild "Release|Any CPU" "VijosNT Mini.sln"
 echo Making RAR archive...
 del pub.rar
 rd pub /s /q
 md pub
 md pub\x86
-copy "bin\x86\release\VijosNT Mini.exe" pub\x86 > nul
+copy "bin\Any CPU\release\VijosNT Mini.exe" pub\x86 > nul
 copy "%ProgramFiles(x86)%\SQLite.NET\bin\System.Data.SQLite.dll" pub\x86 > nul
 md pub\x64
-copy "bin\x64\release\VijosNT Mini.exe" pub\x64 > nul
+copy "bin\Any CPU\release\VijosNT Mini.exe" pub\x64 > nul
 copy "%ProgramFiles(x86)%\SQLite.NET\bin\x64\System.Data.SQLite.dll" pub\x64 > nul
 cd pub
 "%ProgramFiles%\WinRAR\Rar.exe" a -r pub.rar
 cd..
 copy pub\pub.rar > nul
-Rem rd pub /s /q
+rd pub /s /q
 echo All works done.
 echo.
 pause
