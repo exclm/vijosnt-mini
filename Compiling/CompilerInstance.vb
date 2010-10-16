1a406cd589e1b15efbb579e8e524387fcecb0e3e
iceboy <iceboy@sjtu.edu.cn>
2010-10-16 13:22:57 +0000

    removed abstraction about compilers and targets

--- Compiling/CompilerInstance.vb
+++ Compiling/CompilerInstance.vb
@@ -1,15 +1,44 @@
-﻿Namespace Compiling
-    Friend MustInherit Class CompilerInstance
+﻿Imports VijosNT.Utility
+
+Namespace Compiling
+    Friend Class CompilerInstance
         Implements IDisposable
 
-        Public MustOverride ReadOnly Property WorkingDirectory() As String
-        Public MustOverride Function OpenTarget() As Target
-        Public MustOverride ReadOnly Property Compiler() As Compiler
+        Private m_WorkingPath As TempPath
+        Private m_Compiler As Compiler
+
+        Public Sub New(ByVal WorkingPath As TempPath, ByVal Compiler As Compiler)
+            m_WorkingPath = WorkingPath
+            m_Compiler = Compiler
+        End Sub
+
+        Public ReadOnly Property Compiler() As Compiler
+            Get
+                Return m_Compiler
+            End Get
+        End Property
+
+        Public ReadOnly Property WorkingDirectory() As String
+            Get
+                Return m_WorkingPath.GetPath()
+            End Get
+        End Property
+
+        Public Function OpenTarget() As Target
+            Return New Target(New FileStream(m_WorkingPath.Combine(m_Compiler.TargetFileName), FileMode.Open, FileAccess.Read, FileShare.Read), Me)
+        End Function
 
 #Region "IDisposable Support"
+        Private disposedValue As Boolean ' 检测冗余的调用
+
         ' IDisposable
         Protected Overridable Sub Dispose(ByVal disposing As Boolean)
-            ' Do nothing
+            If Not Me.disposedValue Then
+                If disposing Then
+                    m_WorkingPath.Dispose()
+                End If
+            End If
+            Me.disposedValue = True
         End Sub
 
         ' Visual Basic 添加此代码是为了正确实现可处置模式。
